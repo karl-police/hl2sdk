1473c7be7e20d41d8e26056017a36504d2724689
karl-police <karl-police2001@bluewin.ch>
2023-10-02 00:08:06 +0200

    Create entityclass.cpp

---
+++ entity2/entityclass.cpp
@@ -0,0 +1,47 @@
+#include "entitysystem.h"
+
+// In IDA a __int64 can even be a class
+
+
+int64 CEntityClass::UnserializeKey(
+    const CEntityClass::UnserializationContext_t* const context,
+    CEntityInstance* pEntity,
+    char* pFieldMemory,
+    const ComponentUnserializerFieldInfo_t* pFieldInfo,
+    CEntityVariant* value
+/*, __m128d a7*/)
+{
+
+
+}
+
+
+
+// still need to figure out what this returns
+int64 CEntityClass::Unserialize(CEntityInstance* pEntity, const CEntityKeyValues* pKeyValues) {
+
+    EntityIOConnectionDesc_t* m_Size;
+    void** p_outputs;
+
+    UnserializeComponentInfo_t* v8; // keep an eye on this thing
+    EntComponentInfo_t* m_pInfo;
+
+    // Some stuff is only used for logging
+    const char* m_pAsString; // v7, e.g. weapon_glock
+    const char* m_pszCPPClassname; // v9, e.g. CWeaponGlock
+
+    int64 entity_index; // v8, e.g. ent_attachments entity_index
+
+
+
+    CEntityClass* c;
+    CEntityClass::UnserializationContext_t context;
+
+    CUtlVector<CEntityIOOutput*, CUtlMemory<CEntityIOOutput*, int> > outputs;
+    EntityIOConnectionDesc_t desc;
+    CUtlVectorFixedGrowable<UnserializeComponentInfo_t, 64ul> components;
+
+
+    c = this;
+
+}
