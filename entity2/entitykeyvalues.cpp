10e6ed56a37145aa308b7bd1a904fb79c9a3dbc4
karl-police <karl-police2001@bluewin.ch>
2023-10-01 19:48:14 +0200

    Create entitykeyvalues.cpp

---
+++ entity2/entitykeyvalues.cpp
@@ -0,0 +1,38 @@
+#include "entity2/entitysystem.h"
+
+void CEntityKeyValues::ReleaseAllComplexKeys()
+{    
+    for ( EntityComplexKeyListElem_t* i = m_pComplexKeys; i; i = i->m_pNext )
+    {
+        IEntityKeyComplex* pKey = i->m_pKey;
+        while (pKey->m_nRef-- != 1)
+        {
+            i = i->m_pNext;            
+            if (!i)
+                m_pComplexKeys = nullptr;        
+            
+            pKey = i->m_pKey;
+        }
+        
+        pKey->vptr();
+    }
+}
+
+
+void CEntityKeyValues::RemoveAllKeys() {
+    CUtlLeanVectorFixedGrowableBase<unsigned int, 9u, short int>::IndexType_t m_nAllocated;
+    CUtlLeanVectorBase<CEntityKeyValues::KeyValueInfo_t, short int>::ElemType_t* m_pElements;
+    uint16 m_flags;
+}
+
+// CEntityKeyValues::Serialize
+
+
+__int64 CEntityKeyValues::Unserialize(CUtlBuffer* const buf) {
+
+}
+
+
+// CEntityKeyValues::UnserializeKeys from world renderer
+
+// more
