a93a412ccf48664fd4fb12dd3279339d48efeae7
karl-police <karl-police2001@bluewin.ch>
2023-10-02 23:46:35 +0200

    Update entitykeyvalues.h

--- public/entity2/entitykeyvalues.h
+++ public/entity2/entitykeyvalues.h
@@ -3,30 +3,30 @@
 
 #include "entitysystem.h"
 
-struct CEntityKeyValues {
-    CUtlLeanVectorFixedGrowable<unsigned int, 9u, short int> m_keyHashes;
+class CEntityKeyValues {
+public:
+    //CUtlLeanVectorFixedGrowable<unsigned int, 9u, short int> m_keyHashes;
     CUtlLeanVector<CEntityKeyValues::KeyValueInfo_t, short int> m_keyValues;
     CUtlScratchMemoryPool m_memoryPool;
     CEntityKeyValues::EntityComplexKeyListElem_t* m_pComplexKeys;
     int16 m_nRefCount;
     int16 m_nQueuedForSpawnCount;
     CUtlVector<EntityIOConnectionDescFat_t, CUtlMemory<EntityIOConnectionDescFat_t, int> > m_connectionDescs;
-}
 
+    struct CIterator {
+        const CEntityKeyValues* m_pKeyValues;
+        int m_nIndex;
+    };
 
-struct CEntityKeyValues::CIterator {
-    const CEntityKeyValues* m_pKeyValues;
-    int m_nIndex;
-};
+    struct EntityComplexKeyListElem_t {
+        IEntityKeyComplex* m_pKey;
+        CEntityKeyValues::EntityComplexKeyListElem_t* m_pNext;
+    };
 
-struct CEntityKeyValues::EntityComplexKeyListElem_t {
-    IEntityKeyComplex* m_pKey;
-    CEntityKeyValues::EntityComplexKeyListElem_t* m_pNext;
-}
-
-struct CEntityKeyValues::KeyValueInfo_t {
-    CEntityVariant m_value;
-    const char* m_pAttributeName;
+    struct KeyValueInfo_t {
+        CEntityVariant m_value;
+        const char* m_pAttributeName;
+    };
 };
 
 
